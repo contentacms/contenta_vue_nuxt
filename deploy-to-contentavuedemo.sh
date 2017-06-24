774eb9d9b1c12285ed8c9e3068958dec5c5d2f6f
yann <yann.boisselier@gmail.com>
2017-06-24 13:08:39 +0200

    add a no jekyll file

---
+++ deploy-to-contentavuedemo.sh
@@ -0,0 +1,9 @@
+# you need to have a local copy of contentavuedemo.github.io
+yarn generate
+rm -rf ../contentavuedemo.github.io/*
+cp -rf dist/* ../contentavuedemo.github.io
+cd ../contentavuedemo.github.io
+git add . && git commit -m "deploy" && git push
+cd -
+echo "deployment ok"
+
