81a609817def751c951c37501e8ccb647b5c2b29
yann <yann.boisselier@gmail.com>
2017-06-25 14:08:12 +0200

    fix generate config

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,12 +8,6 @@ module.exports = {
   */
   build: {},
   /*
-  ** static generation config
-  */
-  generate:{
-    dir: "docs"
-  },
-  /*
   ** Headers
   ** Common headers are already provided by @nuxtjs/pwa preset
   */
