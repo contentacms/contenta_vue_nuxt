43a8c524ef1b4faeea39c5c0929316bdbad55416
yann <yann.boisselier@gmail.com>
2017-07-03 22:04:54 +0200

    d8 subrequests is now a npm package, lets get back to dev branch

--- pages/index.vue
+++ pages/index.vue
@@ -17,42 +17,6 @@
 import SubRequests from 'd8-subrequests'
 import axios from 'axios'
 export default {
-  transition: 'page',
-  async asyncData () {
-    const subrequests = new SubRequests("https://dev-contentacms.pantheonsite.io/subrequests?_format=json")
-
-    // all categories
-    subrequests.add({
-      uri: "/api/categories"
-    })
-    subrequests.add({
-      uri: "/api/tags"
-    }),
-    subrequests.add({
-      uri: "/api/menus"
-    })
-    // latest 4 recipes
-    subrequests.add({
-      uri: "/api/recipes",
-      options: { 
-        sort:'-created',
-        page: { limit: 10 },
-        include: ['image', 'image.thumbnail'],
-        fields: {
-          recipes:['title', 'difficulty', 'image'],
-          images: ['name', 'thumbnail'],
-          files: ['filename']
-        }
-      }
-    })
-
-    const response = await axios.get(subrequests.getUrl())
-    const datas = subrequests.parseResponse(response.data)
-
-    //  subrequests.send().then(r => console.log(r))
-    
-    return {}
-
-  }
+  transition: 'page'
 }
 </script>
