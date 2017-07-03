79a23daa3c4783fcb5a6534a89045414f398f387
yann <yann.boisselier@gmail.com>
2017-07-03 22:52:21 +0200

    put something on the homepage - latest recipes

--- components/AppVersion.vue
+++ components/AppVersion.vue
@@ -1,5 +1,5 @@
 <template>
    <div class="app-version">
-     <small>0.0.3 - <!--<em> with categories from cache</em>--></small>
+     <small>0.0.4 <!--<em> with categories from cache</em>--></small>
    </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,34 @@
 <template>
-  <section class="hero is-primary">
-    <div class="hero-body">
-      <div class="container">
-        <h1 class="title">
-          UMAMI FOOD MAGAZINE
-        </h1>
-        <h2 class="subtitle">
-          Homepage
-        </h2>
+  <div>
+    <section class="hero is-primary">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">
+            UMAMI FOOD MAGAZINE
+          </h1>
+        </div>
       </div>
+    </section>
+    <div class="container">
+      <br />
+      <h3 class="title is-3">Latest recipes</h3>
+      <recipesAsCards :recipes="recipes"></recipesAsCards>
     </div>
-  </section>
+  </div>
 </template>
 
 <script>
-import SubRequests from 'd8-subrequests'
-import axios from 'axios'
+import Recipes from '~/services/Recipes'
+import RecipesAsCards from '~/components/RecipesAsCards'
+import ButtonLink from '~/components/ButtonLink'
 export default {
-  transition: 'page'
+  transition: 'page',
+  components: { RecipesAsCards },
+  async asyncData ({ params }) {
+    const recipes = await Recipes.findAllLatest(8)
+    return {
+      recipes
+    }
+  }
 }
 </script>
