4a7a2192cc39788aef92640f324b32127f9b2bf9
yann <yann.boisselier@gmail.com>
2017-07-04 17:12:18 +0200

    remove unused page

--- pages/magazine/index.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <section class="content">
-    <div class="container">
-      <h1 class="has-text-centered title is-1">Magazine page</h1>
-      <p> Sorry this page is not yet ready, take a look at the recipes page </p>
-    </div>
-  </section>
-</template>
