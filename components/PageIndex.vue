fd878b547de70cfa99380865c99dd00159637b94
yann <yann.boisselier@gmail.com>
2017-07-04 14:30:12 +0200

    create PageIndex pure component to display homepage

---
+++ components/PageIndex.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="home">
+
+    <section class="hero is-primary">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">
+            UMAMI FOOD MAGAZINE
+          </h1>
+        </div>
+      </div>
+    </section>
+  
+    <MonthEdition></MonthEdition>
+  
+    <section class="home-recipes">
+      <div class="container">
+        <h3 class="title is-3 has-text-centered">Recipes</h3>
+        <h4 class="title is-4 has-text-centered">Explore recipes across every type of occasion, ingredient and skill level</h4>
+        <recipesAsCards :recipes="latestRecipes" cardsByRow="2"></recipesAsCards>
+      </div>
+    </section>
+    
+  </div>
+</template>
+
+<script>
+import RecipesAsCards from '~/components/RecipesAsCards'
+import ButtonLink from '~/components/ButtonLink'
+import MonthEdition from '~/components/MonthEdition'
+
+export default {
+  props: {
+    latestRecipes: {type: Array, default : []}
+  },
+  components: { RecipesAsCards, MonthEdition, ButtonLink }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,41 +1,18 @@
 <template>
-  <div>
-    <section class="hero is-primary">
-      <div class="hero-body">
-        <div class="container">
-          <h1 class="title">
-            UMAMI FOOD MAGAZINE
-          </h1>
-        </div>
-      </div>
-    </section>
-  
-    <MonthEdition></MonthEdition>
-  
-    <section class="home-recipes">
-      <div class="container">
-        <h3 class="title is-3 has-text-centered">Recipes</h3>
-        <h4 class="title is-4 has-text-centered">Explore recipes across every type of occasion, ingredient and skill level</h4>
-        <recipesAsCards :recipes="recipes" cardsByRow="2"></recipesAsCards>
-      </div>
-    </section>
-    
-  </div>
+  <PageIndex :latest-recipes="latestRecipes" />
 </template>
 
 <script>
 import Recipes from '~/services/Recipes'
-import RecipesAsCards from '~/components/RecipesAsCards'
-import ButtonLink from '~/components/ButtonLink'
-import MonthEdition from '~/components/MonthEdition'
+import PageIndex from '~/components/PageIndex'
 
 export default {
   transition: 'page',
-  components: { RecipesAsCards, MonthEdition },
+  components: { PageIndex },
   async asyncData ({ params }) {
-    const recipes = await Recipes.findAllLatest(4)
+    const latestRecipes = await Recipes.findAllLatest(4)
     return {
-      recipes
+      latestRecipes
     }
   }
 }
