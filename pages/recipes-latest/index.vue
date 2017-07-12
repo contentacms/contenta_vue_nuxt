81e3a3925d7efc33db3bb6c1fa613b2e4786a108
yann <yann.boisselier@gmail.com>
2017-07-12 17:24:06 +0200

    fix css card

--- pages/recipes-latest/index.vue
+++ pages/recipes-latest/index.vue
@@ -1,18 +1,20 @@
 <template>
-  <section>
+  <AppSection>
     <div class="container">
       <h3 class="title is-h3 has-text-centered"> Latest recipes </h3>
       <RecipesAsCards title="Latest recipes" :recipes="recipes"></RecipesAsCards>
     </div>
-  </section>
+  </AppSection>
 </template>
 
 <script>
 import Recipes from '~/services/Recipes'
 import RecipesAsCards from '~/components/RecipesAsCards'
+import AppSection from '~/components/AppSection'
+
 export default {
   transition: 'page',
-  components: { RecipesAsCards },
+  components: { RecipesAsCards, AppSection },
   async asyncData ({ params }) {
     const recipes = await Recipes.findAllLatest(20)
     return {
