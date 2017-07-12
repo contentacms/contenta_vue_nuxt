5e78ed9f906befea9e9c9c3f3902f167f4566873
yann <yann.boisselier@gmail.com>
2017-07-12 14:27:14 +0200

    rewrite app plugin with export syntax and using context parameter

--- plugins/app.js
+++ plugins/app.js
@@ -1,11 +1,17 @@
 /**
  * App logic that not belongs to any specific component
  */
-if (process.BROWSER_BUILD) {
-  // we want menu mobile to close when changing page.
-  window.onNuxtReady((app) => {
-    app.$nuxt.$on('routeChanged', (to, from) => {
-      app.$store.commit('setMenuMobileIsOpened', false)
+export default (context) => {
+
+  console.log(context)
+
+  if (context.isClient) {
+    // we want menu mobile to close when changing page.
+    window.onNuxtReady((app) => {
+      app.$nuxt.$on('routeChanged', (to, from) => {
+        app.$store.commit('setMenuMobileIsOpened', false)
+      })
     })
-  })
+  }
+
 }
