20dfee3bdeaca059da4cab213e1c0e77a32a47f2
yann <yann.boisselier@gmail.com>
2017-07-13 23:10:33 +0200

    new page components , increment version

---
+++ components/PageRecipesCategory.vue
@@ -0,0 +1,20 @@
+<template>
+  <AppSection>
+    <div class="container">
+      <h3 class="title is-h3 has-text-centered"> {{ category.name }} </h3>
+      <RecipesAsCards :recipes="recipes"></RecipesAsCards>
+    </div>
+  </AppSection>
+</template>
+
+<script>
+import RecipesAsCards from '~/components/RecipesAsCards'
+import AppSection from '~/components/AppSection'
+export default {
+  components: { RecipesAsCards, AppSection },
+  props: {
+    recipes: { type: Array, default: [] },
+    category: { type: Object, default: {} },
+  }
+}
+</script>
