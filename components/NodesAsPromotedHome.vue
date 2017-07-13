190c821eddb630bbe2e4ad273d350cb2c94ac4db
yann <yann.boisselier@gmail.com>
2017-07-13 16:18:38 +0200

    fix column with for promoted content

--- components/NodesAsPromotedHome.vue
+++ components/NodesAsPromotedHome.vue
@@ -1,26 +1,22 @@
 <template>
   <div class="columns">
-    <div class="column" v-for="node in nodes">
-  
+    <div class="column" :class="{'is-half': index == 0}" v-for="(node, index) in nodes">
+
       <recipeAsCard v-if="node.contentType.type == 'recipe'" :recipe="node" />
       <div v-if="node.contentType.type == 'article'"> There is not yet component to display article as card</div>
-  
+
     </div>
   
   </div>
 </template>
 
 <script>
-import BulmaCard from '~/components/BulmaCard'
 import RecipeAsCard from '~/components/RecipeAsCard'
 
 export default {
-  components: { BulmaCard, RecipeAsCard },
+  components: { RecipeAsCard },
   props: {
     nodes: { type: Array, default: [] }
-  },
-  created () {
-    console.log(this.nodes)
   }
 }
 </script>
