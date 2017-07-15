a2853417d566e4a7223266d28c50af6d54bb56a5
yann <yann.boisselier@gmail.com>
2017-07-15 18:12:57 +0200

    refactor code for navigation test

--- test/unit/specs/AppNavigation.spec.js
+++ test/unit/specs/AppNavigation.spec.js
@@ -1,34 +1,31 @@
 import Vue from 'vue'
 import AppNavigation from '~/components/AppNavigation'
 
-// we have to mock router and our store
+// Mock our router, store and nuxt-link
 import Vuex from 'vuex'
-import storeCreator from '~/store'
+import storeCreate from '~/store'
 import VueRouter from 'vue-router'
+import NuxtLink from '~/.nuxt/components/nuxt-link'
 Vue.use(VueRouter)
 Vue.use(Vuex)
-const mockRouter = new VueRouter({
-  routes: [
-    { path: '/' }
-  ]
-})
-const store = storeCreator()
-import NuxtLink from '~/.nuxt/components/nuxt-link'
 
 /**
  * An example unit test
  */
 describe('AppNavigation.vue', () => {
 
-  it('Mobile menu should have a is-active class when $store.state.menuMobileIsOpened == true', () => {
+  let vm = null
 
-    // Inject Nuxt-link to avoid an error message
+  beforeEach(() => {
+    // Inject manually Nuxt-link to avoid an error message
     AppNavigation.components.NuxtLink = NuxtLink
-
     const Constructor = Vue.extend(AppNavigation)
-    const vm = new Constructor({ router: mockRouter })
-    vm.$store = store
+    vm = new Constructor({ router:  new VueRouter() })
+    vm.$store = storeCreate()
     vm.$mount()
+  })
+
+  it('Mobile menu should have a is-active class when $store.state.menuMobileIsOpened == true', () => {
     expect(vm.$el.querySelector('.nav-right').className.trim()).toBe('nav-right nav-menu')
     vm.setMenuMobileIsOpened()
     // we should now have an active class on next DOM update
