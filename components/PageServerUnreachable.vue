4dc545278e4ac3fa0994ec25953365e22d2c00c8
yann <yann.boisselier@gmail.com>
2017-07-17 16:06:32 +0200

    try to re-trigger github pages deployment

--- components/PageServerUnreachable.vue
+++ components/PageServerUnreachable.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="has-text-centered container">
     <appSection>
-      <h1 class="title is-4">Sorry, API server is unreachable for now </h1>
+      <h1 class="title is-4">Sorry, our API server is unreachable for now </h1>
       <img src="~assets/images/server-unreachable.gif" />
     </appSection>
   </div>
--- deploy-to-github-pages.sh
+++ deploy-to-github-pages.sh
@@ -2,9 +2,9 @@
 
 # fro now you need to have a local copy of contentavuedemo.github.io		
 npm run generate		
-rm -rf ../contentavuedemo.github.io/*		
-cp -r dist/ ../contentavuedemo.github.io		
-cd ../contentavuedemo.github.io		
+rm -rf ../contentavuenuxt.github.io/*		
+cp -r dist/ ../contentavuenuxt.github.io		
+cd ../contentavuenuxt.github.io		
 git add . && git commit -m "deploy" && git push		
 cd -		
-echo "deployment ok"		
+echo "deployment ok"
