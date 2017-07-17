aaabd7a3bf0855a32beed2f4108e40feab097338
yann <yann.boisselier@gmail.com>
2017-07-17 16:08:09 +0200

    fix typo in deploy script

--- deploy-to-github-pages.sh
+++ deploy-to-github-pages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# fro now you need to have a local copy of contentavuedemo.github.io		
+# for now you need to have a local copy of contentavuedemo.github.io		
 npm run generate		
 rm -rf ../contentavuenuxt.github.io/*		
 cp -r dist/ ../contentavuenuxt.github.io		
