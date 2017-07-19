70f0fc46551233e9bffef4eda38fd753d2a282af
yann <yann.boisselier@gmail.com>
2017-07-19 13:44:00 +0200

    feat(App menu): update menu to new navbar bulma menu + css animation
    slide down effect
    
    #8

--- components/AppNavigation.vue
+++ components/AppNavigation.vue
@@ -1,29 +1,31 @@
 <template>
   <div class="container">
-    <nav class="nav">
-      <div class="nav-left">
-        <span class="nav-item">
-          UMAMI food magazine <br />
+    <nav class="navbar">
+      <div class="navbar-brand">
+        <span class="navbar-item">
+          UMAMI food magazine
+          <br />
           <AppVersion />
         </span>
-      </div>
   
-      <!-- This "nav-toggle" hamburger menu is only visible on mobile -->
-      <!-- You need JavaScript to toggle the "is-active" class on "nav-menu" -->
-      <span class="nav-toggle" @click="setMenuMobileIsOpened">
-        <span></span>
-        <span></span>
-        <span></span>
-      </span>
+        <div @click="setMenuMobileIsOpened" :class="{'is-active': this.$store.state.menuMobileIsOpened}" class="navbar-burger burger" data-target="app-menu">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+  
+      </div>
   
       <!-- This "nav-menu" is hidden on mobile -->
       <!-- Add the modifier "is-active" to display it on mobile -->
-      <div class="nav-right nav-menu" :class="{'is-active': this.$store.state.menuMobileIsOpened}">
-        <nuxt-link to="/" class="nav-item"> Home </nuxt-link>
-        <nuxt-link to="/recipes" class="nav-item"> Recipes </nuxt-link>
-        <a href="https://github.com/contentacms/contenta_vue_nuxt" class="nav-item"> Github </a>
-        <a href="http://www.contentacms.org/" class="nav-item"> ContentaCMS website </a>
-        <!--<nuxt-link @click.native="displayMobileMenu = false" to="/magazine" class="nav-item"> Magazine </nuxt-link>-->
+      <div id="app-menu" class="navbar-menu" :class="{'is-active': this.$store.state.menuMobileIsOpened}">
+        <div class="navbar-end">
+          <nuxt-link to="/" class="navbar-item"> Home </nuxt-link>
+          <nuxt-link to="/recipes" class="navbar-item"> Recipes </nuxt-link>
+          <a href="https://github.com/contentacms/contenta_vue_nuxt" class="navbar-item"> Github </a>
+          <a href="http://www.contentacms.org/" class="navbar-item"> ContentaCMS website </a>
+          <!--<nuxt-link @click.native="displayMobileMenu = false" to="/magazine" class="nav-item"> Magazine </nuxt-link>-->
+        </div>
       </div>
   
     </nav>
@@ -35,7 +37,7 @@ import AppVersion from '~/components/AppVersion'
 export default {
   components: { AppVersion },
   methods: {
-    setMenuMobileIsOpened() {
+    setMenuMobileIsOpened () {
       this.$store.commit('setMenuMobileIsOpened', !this.$store.state.menuMobileIsOpened)
     }
   }
@@ -43,7 +45,29 @@ export default {
 </script>
 
 <style scoped>
+
+/* Create slide animation on mobile */
+@media screen and (max-width: 999px) {
+
+  .navbar-end {
+    padding:0.5rem 1rem;
+  }
+
+  .navbar-menu {
+    display: block;
+    overflow: hidden;
+    max-height: 0px;
+    padding: 0rem 1rem;
+    transition: max-height 0.2s;
+  }
+
+  .navbar-menu.is-active {
+    max-height: 100vh;
+    transition: max-height 0.5s;
+  }
+}
+
 .nuxt-link-exact-active {
-  color:#363636;
+  color: #363636;
 }
 </style>
