2a826c389729095da1ac2dd2f797a7e9ab028c88
yann <yann.boisselier@gmail.com>
2017-07-19 12:32:35 +0200

    clean config file

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,23 +12,20 @@ module.exports = {
   ** Build configuration
   */
   build: {
-    // include thoses packages only once to make code lighter
+    // this modules are included in the "vendor" js file
+    // because we use them in every page
     vendor: ['axios', 'waterwheel', 'jsonapi-parse'],
     extend (config) {
       // disable uglify, does not support ES6 -_-
       config.plugins = config.plugins.filter((plugin) => plugin.constructor.name !== 'UglifyJsPlugin')
     }
   },
-  /*
-  ** Headers
-  ** Common headers are already provided by @nuxtjs/pwa preset
-  */
+
   head: {
     meta: [
       {name: 'viewport', content : 'width=device-width, user-scalable=no'}
     ],
     link: [
-      /* @custom */
       { rel: 'stylesheet', type: 'text/css', href: '/css/bulma-4.3.css' },
       { rel: 'stylesheet', type: 'text/css', href: '/css/app.css' }
     ]
@@ -37,12 +34,7 @@ module.exports = {
   ** Customize the progress-bar color
   */
   loading: { color: '#3B8070' },
-  /*
-  ** Customize app manifest
-  */
-  manifest: {
-    theme_color: '#3B8070'
-  },
+  
   /*
   ** Modules
   */
