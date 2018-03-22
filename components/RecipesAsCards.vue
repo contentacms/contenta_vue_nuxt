0c9983a002213ff746189f5936ddf346d1de33f1
yann <yann.boisselier@gmail.com>
2018-03-22 10:30:15 +0100

    fix vue deprecated warning, update packages

--- components/RecipesAsCards.vue
+++ components/RecipesAsCards.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="recipes">
     <BulmaGrid :items="recipes" :itemsByRow="cardsByRow">
-      <template scope="props">
+      <template slot-scope="props">
         <RecipeAsCard :recipe="props.item"></RecipeAsCard>
       </template>
     </BulmaGrid>
@@ -9,17 +9,17 @@
 </template> 
 
 <script>
-import RecipeAsCard from '~/components/RecipeAsCard'
-import BulmaGrid from '~/components/BulmaGrid'
+import RecipeAsCard from "~/components/RecipeAsCard";
+import BulmaGrid from "~/components/BulmaGrid";
 export default {
   components: { RecipeAsCard, BulmaGrid },
   props: {
-    cardsByRow: { type: String, default: "4"},
-    title: { type: String, default: '' },
-    moreLink: { type: String, default: '' },
+    cardsByRow: { type: String, default: "4" },
+    title: { type: String, default: "" },
+    moreLink: { type: String, default: "" },
     recipes: { type: Array, default: () => [] }
   }
-}
+};
 </script>
 
 <style scoped>
