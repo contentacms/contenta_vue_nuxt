bb87cf9d20295668dacfa5f304f2ae2ac57cf0c3
Alex Finnarn <alexfinnarn@gmail.com>
2018-04-23 13:38:07 -0600

    dummy commit to rerun tests

--- components/PageRecipesAll.vue
+++ components/PageRecipesAll.vue
@@ -5,7 +5,7 @@
         <h3 class="title is-h3 has-text-centered">No recipes found.</h3>
       </div>
       <div v-if="recipes.length >= 1">
-        <h3 class="title is-h3 has-text-centered"> All recipes </h3>
+        <h3 class="title is-h3 has-text-centered">All recipes</h3>
         <RecipesAsCards :recipes="recipes"></RecipesAsCards>
       </div>
     </div>
