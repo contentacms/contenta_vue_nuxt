a877cb83714942e100d97a9f1d7367f2b69955ae
yann <yann.boisselier@gmail.com>
2018-10-19 21:04:19 +0200

    simplify nuxt.config.js according to migrations instructions

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,23 +6,9 @@ export default {
     jsonApiPrefix: 'api',
   },
   router: {
-    // check API server is up, to avoid ugly errors pages
+    // check if API server is up, to avoid ugly errors pages
     // when its down or when there is no internet connection
     middleware: 'server-api-available',
-  },
-  /*
-  ** Build configuration
-  */
-  build: {
-    // this modules are included in the "vendor" js file
-    // because we use them in every page
-    vendor: ['axios', 'waterwheel', 'jsonapi-parse'],
-    extend(config) {
-      // disable uglify, does not support ES6 -_-
-      config.plugins = config.plugins.filter(
-        plugin => plugin.constructor.name !== 'UglifyJsPlugin'
-      );
-    },
   },
   head: {
     meta: [
@@ -37,10 +23,4 @@ export default {
   ** Customize the progress-bar color
   */
   loading: { color: '#3B8070' },
-  /*
-  ** Modules
-  */
-  modules: [
-    //  '@nuxtjs/pwa'
-  ],
 };
