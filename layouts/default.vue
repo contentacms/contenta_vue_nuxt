4eb703ca94685a3dc9442c91f8cfbeb46c2e7589
yann <yann.boisselier@gmail.com>
2018-10-27 19:00:57 +0200

    remove unused files

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,19 +1,15 @@
 <template>
   <div>
-    <!--<AppTopBar />-->
     <AppNavigation />
     <nuxt/>
-    <!--<AppFooter />-->
     <AppFooterSocket />
   </div>
 </template>
 
 <script>
 import AppNavigation from '~/components/AppNavigation';
-import AppFooter from '~/components/AppFooter';
 import AppFooterSocket from '~/components/AppFooterSocket';
-import AppTopBar from '~/components/AppTopBar';
 export default {
-  components: { AppNavigation, AppFooter, AppFooterSocket, AppTopBar },
+  components: { AppNavigation, AppFooterSocket },
 };
 </script>
