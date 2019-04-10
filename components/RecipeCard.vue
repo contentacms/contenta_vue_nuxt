349a6414760ce18d308375a3fa45003197ff07c4
Lukas von Blarer <lukas.vonblarer@gmail.com>
2019-04-10 22:54:51 +0200

    Trying to fix image urls

--- components/RecipeCard.vue
+++ components/RecipeCard.vue
@@ -3,7 +3,8 @@
     <BulmaCard>
       <div slot="image" v-if="recipe.image" class="">
         <figure class="image is-3by2">
-          <img class="lazy-img-fadein" v-lazy="`${serverFilesUrl}/${recipe.image.thumbnail.url}`" />
+
+          <img class="lazy-img-fadein" v-lazy="`${serverFilesUrl}/${recipe.image.thumbnail.uri.url}`" />
         </figure>
       </div>
       <div slot="content" class="has-text-centered">
